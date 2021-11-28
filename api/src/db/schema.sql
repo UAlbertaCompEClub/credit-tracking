ac51350ef462cb08b76d0d488c61dfd0b2f07685
rachel-ellis <70451233+rachel-ellis@users.noreply.github.com>
2021-11-28 13:55:53 -0700

    updates

--- api/src/db/schema.sql
+++ api/src/db/schema.sql
@@ -3,27 +3,29 @@
 CREATE TABLE IF NOT EXISTS users (
     ccid TEXT NOT NULL PRIMARY KEY,
     full_name TEXT NOT NULL,
-    foip BOOLEAN,
-    balance REAL
+    isExec BOOLEAN NOT NULL DEFAULT false,
+    foip BOOLEAN NOT NULL DEFAULT false,
+    balance REAL NOT NULL DEFAULT 0
 );
 
 /* execs table */
 CREATE TABLE IF NOT EXISTS execs (
     ccid TEXT NOT NULL PRIMARY KEY,
+    password TEXT NOT NULL,
     program TEXT NOT NULL
 );
 
 /* transations table */
 CREATE TABLE IF NOT EXISTS transactions (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    club TEXT NOT NULL, 
+    club TEXT NOT NULL REFERENCES clubs, 
+    ccid TEXT NOT NULL REFERENCES users,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    amount REAL NOT NULL,
-    ccid TEXT NOT NULL
+    amount REAL NOT NULL,  
 );
 
 /* clubs table */
 CREATE TABLE IF NOT EXISTS clubs (
     name TEXT NOT NULL PRIMARY KEY,
-    amount REAL NOT NULL
+    amount REAL NOT NULL DEFAULT 0
 );
