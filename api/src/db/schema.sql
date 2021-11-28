b4c84227a35e88eb7b390ff3948482bf441ac0be
rachel-ellis <70451233+rachel-ellis@users.noreply.github.com>
2021-11-28 13:24:54 -0700

    updated schema

--- api/src/db/schema.sql
+++ api/src/db/schema.sql
@@ -2,6 +2,7 @@
 /* users table */
 CREATE TABLE IF NOT EXISTS users (
     ccid TEXT NOT NULL PRIMARY KEY,
+    isExec BOOLEAN NOT NULL DEFAULT FALSE,
     full_name TEXT NOT NULL,
     foip BOOLEAN,
     balance REAL
@@ -13,17 +14,29 @@ CREATE TABLE IF NOT EXISTS execs (
     program TEXT NOT NULL
 );
 
+/* clubs table */
+CREATE TABLE IF NOT EXISTS clubs (
+    clubid INT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    amount REAL NOT NULL
+);
+
 /* transations table */
 CREATE TABLE IF NOT EXISTS transactions (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    club TEXT NOT NULL, 
+    clubid INT NOT NULL REFERENCES clubs ON DELETE CASCADE, 
+    ccid TEXT NOT NULL REFERENCES users,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    amount REAL NOT NULL,
-    ccid TEXT NOT NULL
-);
-
-/* clubs table */
-CREATE TABLE IF NOT EXISTS clubs (
-    name TEXT NOT NULL PRIMARY KEY,
     amount REAL NOT NULL
 );
+
+/* update trigger after each transaction (TODO: not working yet!!!)*/
+CREATE TRIGGER update_balance
+AFTER INSERT ON transations FOR EACH ROW
+UPDATE users
+SET    users.balance = users.balance + inserted.amount
+WHERE  users.ccid = inserted.ccid  
+        
+UPDATE clubs
+SET   clubs.amount = clubs.amount - transactions.amount  
+WHERE transactions.clubid = clubs.clubid;
