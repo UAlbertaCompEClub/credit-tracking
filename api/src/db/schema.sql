55544b8c4e930472bc10de681fdee5837e0b8cf2
rachel-ellis <70451233+rachel-ellis@users.noreply.github.com>
2021-11-28 21:50:23 -0700

    fixed trigger

--- api/src/db/schema.sql
+++ api/src/db/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS users (
     ccid TEXT NOT NULL PRIMARY KEY,
     isExec BOOLEAN NOT NULL DEFAULT FALSE,
     full_name TEXT NOT NULL,
-    foip BOOLEAN,
-    balance REAL
+    foip BOOLEAN NOT NULL DEFAULT FALSE,
+    balance REAL NOT NULL DEFAULT 0
 );
 
 /* execs table */
@@ -18,25 +18,36 @@ CREATE TABLE IF NOT EXISTS execs (
 CREATE TABLE IF NOT EXISTS clubs (
     clubid INT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
-    amount REAL NOT NULL
+    amount REAL NOT NULL DEFAULT 0
 );
 
 /* transations table */
 CREATE TABLE IF NOT EXISTS transactions (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    clubid INT NOT NULL REFERENCES clubs ON DELETE CASCADE, 
+    clubid INT NOT NULL REFERENCES clubs, 
     ccid TEXT NOT NULL REFERENCES users,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     amount REAL NOT NULL
 );
 
-/* update trigger after each transaction (TODO: not working yet!!!)*/
-CREATE TRIGGER update_balance
-AFTER INSERT ON transations FOR EACH ROW
-UPDATE users
-SET    users.balance = users.balance + inserted.amount
-WHERE  users.ccid = inserted.ccid  
-        
-UPDATE clubs
-SET   clubs.amount = clubs.amount - transactions.amount  
-WHERE transactions.clubid = clubs.clubid;
+/* Update balance function */
+CREATE OR REPLACE FUNCTION update_balances()
+    RETURNS TRIGGER AS
+$func$
+BEGIN
+    UPDATE users
+    SET balance = balance + NEW.amount
+    WHERE ccid = NEW.ccid;
+
+    UPDATE clubs 
+    SET amount = amount - NEW.amount
+    WHERE clubid = NEW.clubid;
+    RETURN NULL;
+END;
+$func$ LANGUAGE 'plpgsql';
+
+/* trigger to update user and club balance after a transaction */
+CREATE TRIGGER update_balance_from_transaction
+    AFTER INSERT 
+    ON transactions
+    FOR EACH ROW EXECUTE PROCEDURE update_balances();
