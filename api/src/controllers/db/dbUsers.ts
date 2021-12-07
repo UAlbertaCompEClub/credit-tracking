b53186472f36680d7338b3eae9e99b5c041720e7
itsfeas <amf.fiaz@gmail.com>
2021-12-06 22:35:44 -0700

    fixing user creation

--- api/src/controllers/db/dbUsers.ts
+++ api/src/controllers/db/dbUsers.ts
@@ -4,11 +4,12 @@ import type * as schema from 'zapatos/schema';
 import connection from './dbConnection';
 import { encryptPass } from '../../auth/auth';
 
-const createUser = async (userParam: { ccid: string; full_name: string, foip: boolean; }) => {
+const createUser = async (userParam: { ccid: string; full_name: string, foip: boolean, isexec: boolean; }) => {
     const user: schema.users.Insertable = {
         ccid: userParam.ccid,
         full_name: userParam.full_name,
         foip: userParam.foip,
+        isexec: userParam.isexec,
         balance: 0
     };
     db.insert('users', user).run(connection);
