d71354f23b94d5e1049298b027126082625d32ed
itsfeas <amf.fiaz@gmail.com>
2022-02-21 21:14:50 -0700

    added additional middleware for cors access

--- api/src/controllers/util/middleware.ts
+++ api/src/controllers/util/middleware.ts
@@ -22,6 +22,27 @@ function cors_call(): middleware {
     return cors();
 };
 
+// Add headers before the routes are defined
+function cors_access(): middleware {
+    return (req: Request, res: Response, next: NextFunction) => {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+        // Pass to next layer of middleware
+        next();
+    };
+};
+
 
 /**
  *  This is a wrapper that performs exec token checking for routes.
@@ -83,7 +104,8 @@ const secureUser = (f: any) => {
 export default {
     consoleDisplay,
     bodyParser,
-    cors_call
+    cors_call,
+    cors_access
 };
 
 export {
