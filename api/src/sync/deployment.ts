12593c076ce68abb6361e0fa17f2b33b7bfefd9f
itsfeas <amf.fiaz@gmail.com>
2022-04-05 13:48:11 -0600

    fixing invoice queue system

--- api/src/sync/deployment.ts
+++ api/src/sync/deployment.ts
@@ -11,46 +11,43 @@ import type * as schema from 'zapatos/schema';
 */
 const beginDeployment = async () => {
     const deploying = (await stateQueries.getState({ var: "deploying"}))[0];
-    console.log('deploying state:', deploying);
+    stateQueries.updateState({ var: "deploying", val: "1"});
+    const queued = await queueQueries.getQueue();
+    console.log('begin deploying invoices!');
 
-    while (true) {
-        stateQueries.updateState({ var: "deploying", val: "1"});
-        const queued = await queueQueries.getQueue();
-        
-        console.log(queued);
-        var nEmailSent = parseInt((await stateQueries.getState({ var: 'nEmailSent'}))[0]);
-        const clubs = (await basicQueries.getClubs());
-    
-        while(nEmailSent<80 && queued.length!==0) {
-            const user = queued.pop();
-            /* 
-            this version of the code may not send an invoice if
-            the user balances at different clubs cancels out
+    console.log(queued);
+    var nEmailSent = parseInt((await stateQueries.getState({ var: 'nEmailSent' }))[0]);
+    const clubs = (await basicQueries.getClubs());
 
-            EXAMPLE:
-            if the user has a -$10 balance at one club but
-            +$10 balance at another club they will not be
-            sent an invoice.
-            */
-            if (typeof user!== "undefined") {
-                try {
-                    shipInvoice(user.ccid, clubs);
-                    nEmailSent = parseInt((await stateQueries.getState({ var: 'nEmailSent' }))[0]);
-                    stateQueries.updateState({ var: "nEmailSent", val: (nEmailSent + 1).toString() });
-                    queueQueries.removeUser(user.ccid);
-                } catch (error) {
-                    console.log("Email could not be sent for", user.ccid);
-                }
+    while (nEmailSent < 80 && queued.length !== 0) {
+        const user = queued.pop();
+        /* 
+        this version of the code may not send an invoice if
+        the user balances at different clubs cancels out
+
+        EXAMPLE:
+        if the user has a -$10 balance at one club but
+        +$10 balance at another club they will not be
+        sent an invoice.
+        */
+        if (typeof user !== "undefined") {
+            try {
+                shipInvoice(user.ccid, clubs);
+                nEmailSent = parseInt((await stateQueries.getState({ var: 'nEmailSent' }))[0]);
+                stateQueries.updateState({ var: "nEmailSent", val: (nEmailSent + 1).toString() });
+                queueQueries.removeUser(user.ccid);
+            } catch (error) {
+                console.log("Email could not be sent for", user.ccid);
             }
         }
+    }
 
-        if (queued.length === 0) {
-            stateQueries.updateState({ var: "deploying", val: "0" });
-            return;
-        }
-        else {
-            console.log("Will try to continue sending emails tomorrow!");
-        }
+    if (queued.length === 0) {
+        stateQueries.updateState({ var: "deploying", val: "0" });
+        return;
+    }
+    else {
+        console.log("Will try to continue sending emails tomorrow!");
     }
 }
 
